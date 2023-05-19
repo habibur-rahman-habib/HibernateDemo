3fa265dae1f5ea3654edb414d57d26d72ae632d0
habibur-rahman-habib <habibur0199mhr@gmail.com>
2023-05-19 09:19:21 +0600

    tutorial : 6

--- src/main/java/com/habib/test/Test.java
+++ src/main/java/com/habib/test/Test.java
@@ -1,5 +1,9 @@
 package com.habib.test;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -9,26 +13,42 @@ import com.habib.entity.*;
 
 public class Test {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		Configuration cfg = new Configuration();
 		cfg.configure("com/habib/cfgs/hibernate.cfg.xml");
 		SessionFactory factory = cfg.buildSessionFactory();
+		Session session = factory.openSession();
+		Transaction tx = session.beginTransaction();
 		
 		// creating student
-		Student st = new Student();
-		st.setId(101);
-		st.setName("Habibullah Habib");
-		st.setCity("Dhaka");
+//		Student st = new Student();
+//		st.setId(101);
+//		st.setName("Habibullah Habib");
+//		st.setCity("Dhaka");
+//		
+//		System.out.println(st);
+//		
+//		session.save(st);
 		
-		System.out.println(st);
+		// Creating object of Address Class
+		Address ad = new Address();
+		ad.setStreet("street1");
+		ad.setCity("DELHI");
+		ad.setOpen(true);
+		ad.setAddedDate(new Date(1));
+		ad.setX(10);
+		session.save(ad);
 		
-		Session session = factory.openSession();
+		// Reading image
+		FileInputStream fis = new FileInputStream("src/main/java/com/habib/images/star.png");
+		byte[] data = new byte[fis.available()];
+		fis.read(data);
+		ad.setImage(data);
 		
-		Transaction tx = session.beginTransaction();
-		session.save(st);
+		System.out.println(ad);
+		
 		tx.commit();
-		
 		session.close();
 	}
 
